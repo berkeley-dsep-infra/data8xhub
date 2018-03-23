2f63a2f9327db7dd65600ee7342363f0cfeaac03
yuvipanda <yuvipanda@gmail.com>
2018-03-23 11:11:56 -0700

    Save LTI info in a db when launch requests come in

--- images/hubsharder/request-sharder.py
+++ images/hubsharder/request-sharder.py
@@ -5,10 +5,24 @@ from ruamel.yaml import YAML
 
 import os
 from tornado import httpserver, ioloop, web, gen, log, concurrent, httpclient, httputil
+import psycopg2
+import psycopg2.extras
 
 from ltivalidator import LTILaunchValidator, LTILaunchValidationError
 from sharder import Sharder
 
+SCHEMA = """
+CREATE TABLE IF NOT EXISTS lti_launch_info_v1 (
+    id                  SERIAL NOT NULL PRIMARY KEY,
+    user_id             TEXT NOT NULL,
+    resource_link_id    TEXT NOT NULL,
+    launch_info         JSONB NOT NULL,
+    UNIQUE (user_id, resource_link_id)
+);
+CREATE INDEX IF NOT EXISTS user_id_resource_link_id_lti_launch_info_v1 ON lti_launch_info_v1 (
+    user_id, resource_link_id
+)
+"""
 
 class ShardHandler(web.RequestHandler):
     _sharder_thread_pool = ThreadPoolExecutor(max_workers=1)
@@ -16,6 +30,30 @@ class ShardHandler(web.RequestHandler):
     @concurrent.run_on_executor(executor='_sharder_thread_pool')
     def shard(self, username):
         return self.settings['sharder'].shard(username)
+
+    _lti_saver_thread_pool = ThreadPoolExecutor(max_workers=1)
+
+    @concurrent.run_on_executor(executor='_lti_saver_thread_pool')
+    def save_lti_info(self, lti_info):
+        user_id = lti_info['user_id']
+        resource_link_id = lti_info['resource_link_id']
+
+        with self.settings['dbpool'].getconn() as conn:
+            try:
+                with conn.cursor() as cur:
+                    # Make sure that we have at least one dummy entry for each fileserver
+                    cur.execute("""
+                    INSERT INTO lti_launch_info_v1 (user_id, resource_link_id, launch_info)
+                    VALUES (%s, %s, %s)
+                    ON CONFLICT (user_id, resource_link_id)
+                    DO
+                        UPDATE SET launch_info=%s
+                    """, (user_id, resource_link_id, psycopg2.extras.Json(lti_info), psycopg2.extras.Json(lti_info)))
+                    conn.commit()
+                    log.app_log.info(f'Saved lti launch info for user:{user_id} resource_link_id:{resource_link_id}')
+            finally:
+                self.settings['dbpool'].putconn(conn)
+
 
     @gen.coroutine
     def proxy_post(self, path, cluster_ip, hub):
@@ -80,6 +118,8 @@ class ShardHandler(web.RequestHandler):
 
         shard_info = json.loads((yield self.shard(username)))
 
+        yield self.save_lti_info(auth_state)
+
         #self.set_cookie('hub', shard_info['hub'], httponly=True)
         #self.set_cookie('cluster', shard_info['cluster'], httponly=True)
 
@@ -96,9 +136,20 @@ def main():
     # Stringify each line so we can use it as keys
     sharder_buckets = [l for l in json.loads(os.environ['SHARDER_BUCKETS']).split('\n') if l.strip()]
     sharder = Sharder('localhost', username, password, dbname, 'hub', sharder_buckets, log.app_log)
+    dbpool = psycopg2.pool.ThreadedConnectionPool(1, 4, user=username, host='localhost', password=password, dbname=dbname)
+
+    with dbpool.getconn() as conn:
+        try:
+            with conn.cursor() as cur:
+                cur.execute(SCHEMA)
+                conn.commit()
+            log.app_log.info('Finished running schema creation SQL')
+        finally:
+            dbpool.putconn(conn)
+
     application = web.Application([
         (r"/hub/lti/launch", ShardHandler),
-    ], sharder=sharder, consumers=consumers, debug=True)
+    ], sharder=sharder, consumers=consumers, debug=True, dbpool=dbpool)
     http_server = httpserver.HTTPServer(application)
     http_server.listen(8888)
     ioloop.IOLoop.current().start()
