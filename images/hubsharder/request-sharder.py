e1b2ab622771725b2c1d9c16af91dafc7ee4a1b3
yuvipanda <yuvipanda@gmail.com>
2018-03-23 21:23:08 -0700

    Deal with x-forwarded-scheme across multiple hops properly

--- images/hubsharder/request-sharder.py
+++ images/hubsharder/request-sharder.py
@@ -69,11 +69,8 @@ class ShardHandler(web.RequestHandler):
 
         client = httpclient.AsyncHTTPClient()
 
-        headers = {
-            'Cookie': f'hub={hub}',
-            'User-Agent': 'HubSharder',
-            'Host': self.request.host
-        }
+        headers = self.request.headers.copy()
+        headers['Cookie'] = f'hub={hub}'
         req = httpclient.HTTPRequest(
             client_url, method='POST', body=body,
             headers=headers, follow_redirects=False
@@ -110,9 +107,21 @@ class ShardHandler(web.RequestHandler):
             args[k] = values[0].decode() if len(values) == 1 else [v.decode() for v in values]
 
         username = self.get_body_argument('user_id')
+
+        # handle multiple layers of proxied protocol (comma separated) and take the outermost
+        if 'x-forwarded-proto' in self.request.headers:
+            # x-forwarded-proto might contain comma delimited values
+            # left-most value is the one sent by original client
+            hops = [h.strip() for h in self.request.headers['x-forwarded-proto'].split(',')]
+            protocol = hops[0]
+        else:
+            protocol = self.request.protocol
+
+        launch_url = protocol + "://" + self.request.host + self.request.uri
+
         try:
             if validator.validate_launch_request(
-                    self.request.full_url(),
+                    launch_url,
                     self.request.headers,
                     args
             ):
