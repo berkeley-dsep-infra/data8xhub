6abef66b0ec4d364adc72e0e007a130f7e428a66
yuvipanda <yuvipanda@gmail.com>
2018-04-02 02:00:04 -0700

    Set cookies to be not httponly
    And add a username cookie. For convenience, mostly.
    We will try to surface this as an nbextension

--- files/hub-marker.py
+++ files/hub-marker.py
@@ -7,8 +7,9 @@ class CustomAuthenticator(LTIAuthenticator):
     @gen.coroutine
     def authenticate(self, handler, data=None):
         ret = (yield super().authenticate(handler, data))
-        handler.set_cookie('hub', os.environ['HUB_NAME'], httponly=True)
-        handler.set_cookie('cluster', os.environ['CLUSTER_NAME'], httponly=True)
+        handler.set_cookie('hub', os.environ['HUB_NAME'])
+        handler.set_cookie('cluster', os.environ['CLUSTER_NAME'])
+        handler.set_cookie('username', ret['name'])
         return ret
 
 c.JupyterHub.authenticator_class = CustomAuthenticator
