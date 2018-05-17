15b35dc88981bec64043aa8073ca92c18d04b264
yuvipanda <yuvipanda@gmail.com>
2018-05-17 16:15:47 -0700

    Support Dummy / LTI Authenticator properly

--- files/hub-marker.py
+++ files/hub-marker.py
@@ -3,13 +3,17 @@ from dummyauthenticator import DummyAuthenticator
 from tornado import gen
 import os
 
-class CustomAuthenticator(LTIAuthenticator):
+class CustomAuthenticator(DummyAuthenticator):
     @gen.coroutine
     def authenticate(self, handler, data=None):
         ret = (yield super().authenticate(handler, data))
         handler.set_cookie('hub', os.environ['HUB_NAME'])
         handler.set_cookie('cluster', os.environ['CLUSTER_NAME'])
-        handler.set_cookie('username', ret['name'])
+        if isinstance(ret, str):
+            username = ret
+        else:
+            username = ret['name']
+        handler.set_cookie('username', username)
         return ret
 
 c.JupyterHub.authenticator_class = CustomAuthenticator
